6b5c734144defa5b197da1ed8cd51676d915885e
pyeonghwalee <104481496+pyeonghwalee@users.noreply.github.com>
2022-04-27 19:59:14 +0900

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-print("3개의 정수값을 입력하시오")
-n1 = int(input("n1의 정수값을 입력:  "))
-n2 = int(input("n1의 정수값을 입력:  "))
-n3 = int(input("n1의 정수값을 입력:  "))
-
-total = n1 + n2  + n3
-average = total /3;
-print(average)
